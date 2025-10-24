8e20f8f459ee95d31022f0cbbebb1345422d25dd
Luis Felipe <luisfurquijo38@gmail.com>
2025-10-24 14:16:05 -0500

    BASE URL for test

--- ms-admin/tests/acceptance/test_health_smoke.py
+++ ms-admin/tests/acceptance/test_health_smoke.py
@@ -1,7 +1,10 @@
+import os
 from fastapi.testclient import TestClient
 from app.main import app
 
-client = TestClient(app)
+BASE_URL = os.environ.get("APP_BASE_URL", "http://localhost:5000")
+
+client = TestClient(app, base_url=BASE_URL)
 
 
 def test_health_smoke():
